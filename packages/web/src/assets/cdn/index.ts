457cc8706fd2cebdb309315904b73792125c2457
Dshaw <dshaw@foxmail.com>
2026-07-31 14:24:42 +0800

    add cdn index

---
+++ packages/web/src/assets/cdn/index.ts
@@ -0,0 +1,17 @@
+import EMOJI_SPRITE from "@/assets/emoji-sprite.png";
+import PNQQK6 from "./pnqqk6.jpg";
+import PNQXLD from "./pnqxld.jpg";
+import PNQZ5X from "./pnqz5x.jpg";
+import PNR1BD from "./pnr1bd.jpg";
+import PNR1OF from "./pnr1of.jpg";
+import PNR4UB from "./pnr4ub.jpg";
+
+export {
+	EMOJI_SPRITE,
+	PNQQK6,
+	PNQXLD,
+	PNQZ5X,
+	PNR1BD,
+	PNR1OF,
+	PNR4UB,
+};
